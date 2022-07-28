41ea5cc11ca1c870ea7be38d0a2a5320575e2f82
Chad Slater <89958756+chad-slater@users.noreply.github.com>
2022-07-28 21:09:10 +1000

    run full stack concurrently

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send("Hello, world!");
+app.get("/api", (req, res) => {
+  res.json({ hello: "world" });
 });
 
 app.listen(PORT, () => {
--- frontend/src/components/App/index.js
+++ frontend/src/components/App/index.js
@@ -1,8 +1,18 @@
 const App = () => {
+  greeting();
+
   return (
     <>
       <p>Hello, world!</p>
     </>
   );
 };
+
+const greeting = async () => {
+  const response = await fetch("/api");
+  const data = await response.json();
+
+  console.log(data);
+};
+
 export default App;
