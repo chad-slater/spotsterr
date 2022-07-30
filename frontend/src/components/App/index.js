7c7c40b60fe5d28aec45680ba606473d10f56b53
Chad Slater <89958756+chad-slater@users.noreply.github.com>
2022-07-31 09:37:00 +1000

    spotify auth working

--- frontend/src/components/App/index.js
+++ frontend/src/components/App/index.js
@@ -1,18 +1,25 @@
+const axios = require("axios").default;
+
 const App = () => {
-  greeting();
+  const accessToken = document.cookie.split("=")[1];
+
+  axios("https://api.spotify.com/v1/tracks/2TpxZ7JUBn3uw46aR7qd6V", {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 
   return (
     <>
-      <p>Hello, world!</p>
+      <a href="http://localhost:5000/api/login">Log in to Spotify</a>
     </>
   );
-};
-
-const greeting = async () => {
-  const response = await fetch("/api");
-  const data = await response.json();
-
-  console.log(data);
 };
 
 export default App;
