6a5c3425b5df8be6ca1e32701385ee340636fea5
chad <slater.chad+incode@outlook.com>
2022-08-04 13:39:06 +1000

    prepare heroku deployment

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const { errorHandler } = require("./middleware/errorMiddleware");
 const cookieParser = require("cookie-parser");
 const express = require("express");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -11,6 +12,19 @@ app.use(express.json());
 
 app.use("/api/songsterr", require("./routes/songsterrRoutes"));
 app.use("/api/spotify", require("./routes/spotifyRoutes"));
+
+// Serve frontend
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "..", "frontend", "build", "index.html")
+    )
+  );
+}
+
+app.get("/", (req, res) => res.send("Please set environment to production"));
 
 app.use(errorHandler);
 
