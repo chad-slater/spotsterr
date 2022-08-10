9070c70144a71a8d483e134f968e6d0f84f80182
Chad Slater <89958756+chad-slater@users.noreply.github.com>
2022-08-10 13:02:16 +1000

    404

--- frontend/src/components/FourOhFour/index.js
+++ frontend/src/components/FourOhFour/index.js
@@ -1,4 +1,20 @@
 const FourOhFour = () => {
-  return <div>404 Page Not Found</div>;
+  return (
+    <>
+      <div className="flex flex-col h-330px items-center my-4 w-full sm:flex-row sm:justify-evenly sm:my-8">
+        <img
+          className="border border-1 border-black w-1/3"
+          src="/img/404.png"
+          alt="404"
+        />
+        <div className="flex flex-col justify-between m-4 text-center sm:text-left">
+          <h1 className="font-bold my-2 text-4xl sm:text-5xl lg:text-6xl">
+            Page Not Found
+          </h1>
+          <p>The Internet</p>
+        </div>
+      </div>
+    </>
+  );
 };
 export default FourOhFour;
